cf2f24f864b6aaccaae653429cf5af70cdcd9498
Lutfullah Öztürk <lutfullah_06@hotmail.com>
2021-06-17 08:19:59 +0300

    Add project files.

---
+++ UserManagementScreen/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UserManagementScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // In order to access the database, you need to change the connection string below.
+        public string constr = "Data Source=.;Initial Catalog=piworks;Integrated Security=True";
+        SqlCommand cmd = new SqlCommand();
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox2.Enabled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection sqlcon = new SqlConnection(constr);
+            cmd.Connection = sqlcon;
+            cmd.CommandText = "INSERT INTO UIManagement(Username,Displayname,Phone,Email,UserRoles,Enabled) " +
+                "VALUES('"+textBox1.Text+"','" + textBox2.Text+"','" + textBox3.Text+"','" + textBox4.Text+"','" + comboBox1.SelectedItem+"','"+checkBox2.Checked.GetHashCode()+"')";
+            sqlcon.Open();
+            int result = cmd.ExecuteNonQuery();
+            label7.Text = result.ToString() + " User info added to Database";
+
+            showdata("select * from UIManagement");
+        }
+
+        public void showdata(string datafromsql) 
+        {
+            SqlDataAdapter da = new SqlDataAdapter(datafromsql, constr);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(constr);
+            sqlcon.Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ UserManagementTool/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserManagementTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataColumn column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.AutoIncrement = true;
+            column.AutoIncrementSeed = 1;
+            column.AutoIncrementStep = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.Rows.Add();
+
+            i = 1;
+            dataGridView1.Rows[i].Cells[0].Value = i;
+            dataGridView1.Rows[i].Cells[1].Value = textBox1.Text;
+            dataGridView1.Rows[i].Cells[2].Value = textBox2.Text;
+            dataGridView1.Rows[i].Cells[3].Value = textBox3.Text;
+   
+            i++;
+
+        }
+    }
+}
